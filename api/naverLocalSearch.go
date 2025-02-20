19d4e4c186c15306043e1d914388cc3bcd8364ca
이태훈 <rhymint@gmail.com>
2025-02-20 15:14:40 +0900

    style: TODO 주석 추가

--- api/naverLocalSearch.go
+++ api/naverLocalSearch.go
@@ -11,6 +11,7 @@ type NaverLocalSearchHandler struct {
 	Service *services.LocalSearchService
 }
 
+//TODO: naverLocalSearch Cache 추가
 func NewNaverLocalSearchHandler(service *services.LocalSearchService) *NaverLocalSearchHandler {
 	return &NaverLocalSearchHandler{Service: service}
 }
